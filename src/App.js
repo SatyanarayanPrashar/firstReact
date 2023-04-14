a966cb915f4b671956f969e3725b27b92e847719
Mr-MeowMeow <satyanarayanprashar73@gmail.com>
2023-04-15 04:16:41 +0530

    dark mode completed

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
-// import AboutPg from './components/AboutPg';
+import AboutPg from './components/AboutPg';
+import React, { useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 
 function App() {
+
+  const [mode, setMode] =useState('light');
+  const toggleMode = ()=>{
+    if(mode==='light'){
+      setMode('dark');
+      document.body.style.backgroundColor = '#042743';
+    } else{
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+    }
+  }
+
   return (
   <>
-    <Navbar title="Edloops" aboutText="About"/>
+    <Navbar title="Edloops" mode={mode} toggleMode={toggleMode}/>
     <div className="container">
-      <TextForm heading="Enter a text to analyze below"/>
-      {/* <AboutPg/> */}
+      <TextForm heading="Enter a text to analyze below" mode={mode}/>
+      <AboutPg/>
     </div>
   </>
   );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,42 +1,42 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 
 
 export default function TextForm(props) {
     const [text, setText] = useState('');
 
-    const handleUpClick = () =>{
+    const handleUpClick = () => {
         console.log("upper case clicked");
         let newText = text.toUpperCase();
         setText(newText);
     }
-    const handleLowClick = () =>{
+    const handleLowClick = () => {
         console.log("Lower case clicked");
         let newText = text.toLowerCase();
         setText(newText);
     }
-    
-    const handleOnChange = (event) =>{
+
+    const handleOnChange = (event) => {
         console.log("On change");
         setText(event.target.value);
     }
-    
+
     return (
         <>
-        <div>
-            <h1>{props.heading}</h1>
-            <div className="mb-3">
-                <textarea className="form-control" id="myBox" rows="6" value={text} onChange={handleOnChange}></textarea>
+            <div className='container' style={{ color: props.mode === 'light' ? 'black' : 'white' }}>
+                <h1>{props.heading}</h1>
+                <div className="mb-3">
+                    <textarea className="form-control" style={{ backgroundColor: props.mode === 'light' ? 'white' : 'grey', color: props.mode === 'light' ? 'black' : 'white' }} id="myBox" rows="6" value={text} onChange={handleOnChange}></textarea>
+                </div>
+                <button className='btn btn-primary' onClick={handleUpClick}>Convert to Uppercase</button>
+                <button className='btn btn-primary mx-3' onClick={handleLowClick}>Convert to Lowercase</button>
             </div>
-            <button className='btn btn-primary' onClick={handleUpClick}>Convert to Uppercase</button>
-            <button className='btn btn-primary mx-3' onClick={handleLowClick}>Convert to Lowercase</button>
-        </div>
-        <div className="container my-3">
-            <h1>Your Text Summary</h1>
-            <p>{text.split(" ").length} words and {text.length} characters</p>
-            <p>{0.008 * text.split(" ").length} min read</p>
-            <h2>Preview</h2>
-            <p>{text}</p>
-        </div>
+            <div className="container my-3" style={{ color: props.mode === 'light' ? 'black' : 'white' }}>
+                <h1 >Your Text Summary</h1>
+                <p>{text.split(" ").length} words and {text.length} characters</p>
+                <p>{0.008 * text.split(" ").length} min read</p>
+                <h2>Preview</h2>
+                <p>{text}</p>
+            </div>
         </>
     )
 }
